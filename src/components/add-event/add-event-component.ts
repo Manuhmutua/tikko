ef41e758877855a16e4a10bb040b9a223fee12a4
Manuh.__ <manuh.mutuah@gmail.com>
2021-07-24 21:30:12 +0300

    Added image url on event object

--- src/components/add-event/add-event-component.ts
+++ src/components/add-event/add-event-component.ts
@@ -9,7 +9,7 @@ const eventdate = ref("");
 const eventtime = ref("");
 const eventdescription = ref("");
 const imageName = ref("");
-const percentage = ref("");
+const percentage = ref();
 
 export default {
   data() {
@@ -25,10 +25,12 @@ export default {
       const fileList = val.target.files;
       console.log(fileList);
 
-      imageName.value = `img/${auth.currentUser.uid}/${eventname.value.replace(
-        /\s/g,
-        ""
-      )}/${fileList[0].name}`;
+      const storage = firebase.storage();
+
+      // imageName.value = `img/${auth.currentUser.uid}/${eventname.value.replace(
+      //   /\s/g,
+      //   ""
+      // )}/${fileList[0].name}`;
 
       console.log(auth.currentUser.uid);
 
@@ -43,9 +45,8 @@ export default {
       task.on(
         "state_changed",
         function progress(snapshot) {
-          let p =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            percentage.value = Math.round(p)
+          let p = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          percentage.value = Math.round(p);
           console.log(percentage.value);
           console.log(snapshot);
         },
@@ -54,6 +55,20 @@ export default {
         },
         function complete() {
           console.log(imageName.value);
+
+          storage
+            .ref(
+              `img/${auth.currentUser.uid}/${eventname.value.replace(
+                /\s/g,
+                ""
+              )}/${fileList[0].name}`
+            )
+            .getDownloadURL()
+            .then((url) => {
+              // Do something with the URL ...
+              console.log(url);
+              imageName.value = url;
+            });
         }
       );
     }
@@ -66,7 +81,7 @@ export default {
       eventtime,
       eventdescription,
       imageName,
-      percentage
+      percentage,
     };
   },
   methods: {
