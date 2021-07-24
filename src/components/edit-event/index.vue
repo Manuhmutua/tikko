adcae0faca95d043f7e8f7894216279cd0e2ce31
Manuh.__ <manuh.mutuah@gmail.com>
2021-07-24 21:18:04 +0300

    Added view and edit event

---
+++ src/components/edit-event/index.vue
@@ -0,0 +1,5 @@
+<template src="./edit-event-component.html"></template>
+
+<script src="./edit-event-component.ts"></script>
+
+<style src="./edit-event-component.css" scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,41 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Login from '../components/login/index.vue'
-import Dashboard from '../components/dashboard/index.vue'
-import AddEvent from '../components/add-event/index.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "../components/login/index.vue";
+import Dashboard from "../components/dashboard/index.vue";
+import AddEvent from "../components/add-event/index.vue";
+import ViewEvent from "../components/view-event/index.vue";
+import EditEvent from "../components/edit-event/index.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Login',
-    component: Login
+    path: "/",
+    name: "Login",
+    component: Login,
   },
   {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: Dashboard
+    path: "/dashboard",
+    name: "Dashboard",
+    component: Dashboard,
   },
   {
-    path: '/add-event',
-    name: 'AddEvent',
-    component: AddEvent
-  }
-]
+    path: "/add-event",
+    name: "AddEvent",
+    component: AddEvent,
+  },
+  {
+    path: "/view-event",
+    name: "ViewEvent",
+    component: ViewEvent,
+  },
+  {
+    path: "/edit-event",
+    name: "EditEvent",
+    component: EditEvent,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
